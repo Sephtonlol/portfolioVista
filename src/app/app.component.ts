34139bc1a548487bf1922b5debdef792533709e3
Alexander <alexanderwu271206@gmail.com>
2024-11-20 00:00:54 +0100

    Added nav links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectInfoComponent } from './components/project-info/project-info.component';
-
+import { NavigationComponent } from './components/navigation/navigation.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    NavigationComponent,
     HeaderComponent,
     ProjectsComponent,
     ProjectInfoComponent,
