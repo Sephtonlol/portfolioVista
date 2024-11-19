6efc2caf978c75678f666d94b05f325adeed5b42
Alexander <alexanderwu271206@gmail.com>
2024-11-19 21:00:23 +0100

    Added components & header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectInfoComponent } from './components/project-info/project-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProjectsComponent,
+    ProjectInfoComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolioVista';
